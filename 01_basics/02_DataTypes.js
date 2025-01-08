f15279e0b3f9dec11e9093591d36fac56b12a720
Nitish-ydv <167611635+Nitish-ydv@users.noreply.github.com>
2025-01-08 08:32:42 +0000

    datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //teat all js code as newer version 
+ 
+// alert(3+3) we are using node js not browser 
+
+let name = "Nitish kumar "
+let age = 21
+let isLoggined = false
+
+console.log(name)
+
+// number => 2 to power 53
+// bigInt 
+// string => "Nitish kumar " single quota '' or double ""
+// boolean => true or false 
+// null => standalone value
+// undefined => value undefined
+//  symbol => unique 
+
+
+//object 
+
+
+
+console.log(typeof "Nitish" )
