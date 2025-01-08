aa2514ace298c08666605eae2b0984ed116b7c5e
Nitish-ydv <167611635+Nitish-ydv@users.noreply.github.com>
2025-01-08 08:33:34 +0000

    basics variable

---
+++ 01_basics/01_basic_Variable.js
@@ -0,0 +1,19 @@
+console.log("Nitish kumar")
+
+
+// const never change 
+
+
+const accountId = 9341
+let accountEmail = "nitishydv@gmail.com"
+var accountPassword = "9341172161"
+accountCity = "Patna"
+let accountState;
+/*  prefer not to use var because of some issue
+like issue in block scope and functional scope
+*/
+
+// accountId = 2
+console.log(accountId)
+
+console.table([accountEmail,accountId,accountPassword,accountState])
