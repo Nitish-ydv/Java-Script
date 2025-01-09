6bafc2604586dd3ee103de05aa3c19714b43e854
Nitish-ydv <167611635+Nitish-ydv@users.noreply.github.com>
2025-01-09 16:24:03 +0000

    conversion and operation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,23 @@
+let number = "65"
+
+console.log (typeof number)
+console.log(typeof( number))
+
+
+let valueInNumber = Number(number)
+console.log(typeof valueInNumber)
+
+
+// "65" => 
+//  65gjj = NAN
+//true = 1  ,  false = 0
+
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+console.log( booleanisloggedin)
+
+
+
+// 1 => true  , 0=> false
+// " " => false
+// "Nitsh" => true
